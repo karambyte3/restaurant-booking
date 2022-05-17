ff42cd5842c058e4fe90b55985e10b109cf1921b
Stefan Kalenderov <stefankalenderov@gmail.com>
2022-05-18 00:25:28 +0300

    added smooth scroll, spy scroll
    optimized & formatted main.js
    added smooth scroll, spy scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,51 +1,84 @@
 'use strict'
 
-$(document).ready(function () {
+// menu slide animation
+var lastScrollTop = 0;
+function menuAnimation () {
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    var $navMenuEl = $('.nav-menu');
+    if (st > lastScrollTop){ // scroll down
+        $navMenuEl.addClass('collapsing-menu-down');
+        setTimeout(function () {
+            $navMenuEl.removeClass('collapsing-menu-down');
+            $navMenuEl.addClass('scrolledIn');
+        }, 300);
+    } else { // scroll up
+        $navMenuEl.addClass('collapsing-menu-up');
+
+        setTimeout(function () {
+            $navMenuEl.removeClass('collapsing-menu-up');
+            $navMenuEl.removeClass('scrolledIn');
+        }, 300);
+    }
+    lastScrollTop = st;
+}
+
+// SCROLL SPY
+function scrollSpyNavigation () {
+    $('body').scrollspy({target: ".navbar"});
+
+    var currentTop = $(window).scrollTop();
+    var elems = $('.scrollspy');
+    var elementTopSpace = 350;
+    elems.each(function () {
+        var elemTop = $(this).offset().top - elementTopSpace;
+
+        if (currentTop > elemTop) {
+            var id = $(this).attr('id');
+            var navElem = $('a[href="#' + id + '"]');
+            navElem.addClass('active');
+            navElem.parent().siblings().find('a').removeClass('active');
+        }
+    })
+
+    if ($(elems[0]).offset().top > currentTop + elementTopSpace) {
+        $('.nav-menu').find('a.active').removeClass('active');
+    }
+}
+
+function smoothScrollAnimation() {
+    $('body').on('click', '.smooth-scroll', function (e) {
+        e.preventDefault();
+        var $this = $(this);
+
+        $this.removeClass('active');
+        $this.addClass('active');
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100}, 1000, 'easeInOutQuart');
+    });
+}
+
+function initParallax() {
+    if (typeof window !== 'undefined') { new window.simpleParallax()}
+    var images = document.querySelectorAll('.parallax');
+    new window.simpleParallax(images);
+}
+
+function initSlickSlider() {
     $('.dishes-slider').slick({
         dots: true,
         infinite: true,
         arrows: true,
         // speed: 300,
         slidesToShow: 2
-      });
+    });
+}
 
-      $(window).scroll(function() {
-
-        });
-
-    var lastScrollTop = 0;
+$(document).ready(function () {
     $(window).on('scroll', function () {
-        $("#skin_h").removeClass("skin");
-
-        var st = window.pageYOffset || document.documentElement.scrollTop;
-        if (st > lastScrollTop){ // scroll down
-            $('.nav-menu').addClass('collapsing-menu-down');
-            setTimeout(function () {
-                $('.nav-menu').removeClass('collapsing-menu-down');
-                $('.nav-menu').addClass('scrolledIn');
-            }, 300);
-        } else { // scroll up
-            $('.nav-menu').addClass('collapsing-menu-up');
-
-            setTimeout(function () {
-                $('.nav-menu').removeClass('collapsing-menu-up');
-                $('.nav-menu').removeClass('scrolledIn');
-            }, 300);
-        }
-        lastScrollTop = st;
-    })
-
-    $('body').on('click', '.smooth-scroll', function (e) {
-        e.preventDefault();
-        $('.smooth-scroll').removeClass('active');
-        $(this).addClass('active');
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100}, 1000, 'easeInOutQuart');
+        menuAnimation();
+        scrollSpyNavigation();
     });
 
-    $('body').scrollspy({target: ".navbar"})
-
-    if (typeof window !== 'undefined') { new window.simpleParallax()}
-    var images = document.querySelectorAll('.parallax');
-    new window.simpleParallax(images);
-
+    smoothScrollAnimation();
+    initParallax();
+    initSlickSlider();
 });
